6965fd88d650a755b64d2e4f16408e35c0a78f5d
zolotoy <romprogramist@gmail.com>
2022-12-19 22:06:29 +0400

    "vsio"

--- Vertex/wwwroot/js/home.js
+++ Vertex/wwwroot/js/home.js
@@ -5,69 +5,87 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     const pluss = document.querySelectorAll('.plus');
     const minus = document.querySelectorAll('.minus');
-    
-    let caunt = 0;
-    let pricePlast = 7500;
-    let pricePlastDiscont10 = 6700;
-    let pricePlastDiscont5 = 7000;
+
     let sum = 0;
     let multiplication = 0;
     
-    function plussFun (price1, price2, price3) {
+    function plussFun () {
         pluss.forEach(el => {
             el.addEventListener('click', () => {
+                let caunt = parseInt(el.nextElementSibling.textContent);
                 el.nextElementSibling.innerHTML = '';
                 caunt += 1
                 el.nextElementSibling.innerHTML = caunt;
+                let orignPrice = parseInt(el.parentNode.parentNode.firstElementChild.firstElementChild.textContent);
                 for(let i = 1; i <= caunt; i++) {
                     multiplication = i
                 }
                 if(multiplication < 5) {
-                    sum = multiplication * price1;
+                    sum = multiplication * orignPrice;
                 }
                 if(multiplication >= 5) {
-                    sum = price2 * multiplication;
+                    let dis5 = orignPrice / 100 * 5
+                    let sumDis5 = orignPrice - dis5;
+                    sum = sumDis5 * multiplication;
                 }
                 if(multiplication >= 10) {
-                    sum = price3 * multiplication;
+                    let dis5 = orignPrice / 100 * 9
+                    let sumDis10 = orignPrice - dis5;
+                    sum = sumDis10 * multiplication;
                 }
                 el.nextElementSibling.nextElementSibling.nextElementSibling.innerHTML = sum;
             })
         })
     }
-    plussFun( pricePlast, pricePlastDiscont5, pricePlastDiscont10 );
-    function minusFun(price1, price2, price3) {
+    plussFun( );
+    function minusFun() {
         minus.forEach(el => {
             el.addEventListener('click', () => {
+                let caunt = parseInt(el.previousElementSibling.textContent);
+                let orignPrice = parseInt(el.parentNode.parentNode.firstElementChild.firstElementChild.textContent);
                 el.previousElementSibling.innerHTML = '';
                 caunt -= 1
                 multiplication--
-
                 if(caunt <= 0) {
                     caunt = 0
                 }
-
                 if(multiplication <= 4) {
-                    sum = sum - price1;
-                    sum = multiplication * price1;
+                    sum = sum - orignPrice;
+                    sum = multiplication * orignPrice;
                 }
                 if(multiplication >= 5 && multiplication <= 9) {
-                    sum = sum - price2
-                    sum = price2 * multiplication;
+                    let dis5 = orignPrice / 100 * 5
+                    let sumDis5 = orignPrice - dis5;
+                    sum = sum - sumDis5
+                    sum = sumDis5 * multiplication;
                 }
-
                 if(multiplication >= 10) {
-                    sum = sum - price3
-                    sum = price3 * multiplication;
+                    let dis5 = orignPrice / 100 * 10
+                    let sumDis5 = orignPrice - dis5;
+                    sum = sum - sumDis5
+                    sum = sumDis5 * multiplication;
                 }
                 if(sum <= 0) sum = 0;
-
                 el.nextElementSibling.innerHTML = sum;
                 el.previousElementSibling.innerHTML = caunt;
 
             })
         })
     }
-    minusFun( pricePlast, pricePlastDiscont5, pricePlastDiscont10 );
     // code here
+    minusFun()
+    
+    document.querySelector('.m-trigger').addEventListener('click', () => {
+        let cauntPl = document.querySelector('.plastic .caunt');
+        let cauntMn = document.querySelector('.monomer .caunt');
+        document.querySelector('.plasticModal').innerHTML = cauntPl.textContent;
+        document.querySelector('.monomerModal').innerHTML = cauntMn.textContent;
+    })
+    
+    
 });
+
+
+
+
+
